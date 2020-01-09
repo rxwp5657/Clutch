263555908fa1519401300204b0b0449d3229e929
Juan Carlos Sanchez Ruiz <juancarlossanchezruiz@MacBook-Pro-de-Juan.local>
2020-01-09 02:49:17 -0600

    deleting build files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/juancarlossanchezruiz/Documents/Proyects/Clutch/test
-# Build directory: /Users/juancarlossanchezruiz/Documents/Proyects/Clutch/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest_bin_dir")
--- build/gtest_bin_dir/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/juancarlossanchezruiz/Documents/Proyects/Clutch/lib/gtest
-# Build directory: /Users/juancarlossanchezruiz/Documents/Proyects/Clutch/build/gtest_bin_dir
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
